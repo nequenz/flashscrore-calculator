966b3c26880a5bb30f92d4ea2080f6c3265763eb
Slava <70316676+nequenz@users.noreply.github.com>
2023-03-25 00:10:15 +0300

    Add files via upload

--- finn.js
+++ finn.js
@@ -21,7 +21,7 @@ function GetScores( name )
 		values = Games[ i ].innerText.match(/\d+/gi);
 		gameResult = [];
 		
-		if( values == null )
+		if( values === null )
 			continue;
 		
 		values.forEach( ( strValue ) => 
@@ -52,14 +52,14 @@ function CalculateOddEven( scores, noValues )
 		{
 			sumValue += value;
 			
-			if( index % 2 == 1)
+			if( index % 2 === 1)
 			{
-				if( noValues == false )
-					gameResult.push( ( sumValue % 2 == 1 ? "Н" : "Ч") + " (" +sumValue + ")" );
+				if( noValues === false )
+					gameResult.push( ( sumValue % 2 === 1 ? "Н" : "Ч") + " (" +sumValue + ")" );
 				else
-					gameResult.push( ( sumValue % 2 == 1 ? "Н" : "Ч") );
+					gameResult.push( ( sumValue % 2 === 1 ? "Н" : "Ч") );
 				
-				if( sumValue % 2 == 1 )
+				if( sumValue % 2 === 1 )
 					OddEvenValues[ 0 ] += 1;
 				else
 					OddEvenValues[ 1 ] += 1;
@@ -97,13 +97,13 @@ function AcceptRule( rule, games, isOffsetable, cash )
 			gameValue = game[ k ];
 			ruleValue = rule[ ( count - offset ) % rule.length];
 			
-			if( ruleValue == gameValue )
+			if( ruleValue === gameValue )
 			{
 				cash.Play( true );
 				
 				resultText += "+ *** pos:" + count + ", Game Value: " + gameValue + "  Rule Value: " + ruleValue + "\n";
 				
-				if( isOffsetable == true )
+				if( isOffsetable === true )
 					offset = count + 1;
 			}
 			else
@@ -155,6 +155,8 @@ class Cash
 	Factor;
 	Steps;
 	Step;
+	Count;
+	MaxCash;
 	
 	constructor( cash, order, factor, steps )
 	{
@@ -166,12 +168,17 @@ class Cash
 		this.NeededCashDec = this.Order / this.Steps;
 		this.CurrentCash = this.StartCash;
 		this.Step = 0;
+		this.Count = 1;
+		this.MaxCash = this.StartCash;
 	}
 	
 	Play = function( isWin )
 	{
 		let playableCash = ( this.NeededCash - this.CurrentCash ) / ( this.Factor - 1 );
 		let color;
+		
+		if( this.CurrentCash <= 0 )
+			return;
 		
 		this.CurrentCash -= playableCash;
 		
@@ -191,16 +198,66 @@ class Cash
 		}
 		else
 		{
-			this.NeededCash -= ( this.Step > 0 ) ? this.NeededCashDec : 0;
+			this.NeededCash -= this.NeededCashDec;
 			this.Step ++;
 			color = "background: #222; color: red";
+			
+			if( this.Step > this.Steps )
+			{
+				this.NeededCash = this.CurrentCash + this.Order;
+				this.Step = 0;
+				
+				console.log( "Step:" + this.Step +", Steps:" + this.Steps );
+			}
 		}
 
-		console.log( "%cCurrent cash:" + this.CurrentCash.toFixed(2) + " C, Needed cash:" + this.NeededCash.toFixed(2) + " C, Play cash:" + playableCash.toFixed(2) + " C", color );
+		console.log( "%c#" + this.Count + " Current cash:" + this.CurrentCash.toFixed(2) + " C, Needed cash:" + this.NeededCash.toFixed(2) + " C, Play cash:" + playableCash.toFixed(2) + " C", color );
+		this.Count ++;
 	}
 } 
 
 
-//let TCash = new Cash( 5000, 100, 1.85, 5 );
 
-AcceptRule( "Ч", CalculateOddEven( GetScores("basketball"), true ), false, new Cash( 5000, 100, 1.85, 5 ) );
+//AcceptRule( "Ч", CalculateOddEven( GetScores("basketball"), true ), false, new Cash( 5000, 100, 1.85, 5 ) );
+
+
+//---------------------------------------------
+
+function GetFactors( game )
+{
+	const First = game.children[ game.children.length - 2 ];
+	const Second = game.children[ game.children.length - 1 ];
+	let FFactor = First.title.match(/[\d.]+/gi);
+	let SFactor = Second.title.match(/[\d.]+/gi);
+	let FResult = First.className.search("win") >= 0 ? "+" : "-";
+	let SResult = Second.className.search("win") >= 0 ? "+" : "-";
+	
+	if(FFactor != null)
+		FFactor = parseFloat( FFactor[ 0 ] );
+	
+	if(SFactor != null)
+		SFactor = parseFloat( SFactor[ 0 ] );
+	
+	return [FFactor, SFactor, FResult, SResult];
+}
+
+function FilterGames( games )
+{
+	let game;
+	let result;
+	const List = [];
+	
+	for( let i = 0; i < games.length; i ++ )
+	{
+		game = games[ i ];
+		result = GetFactors( game );
+		
+		if( ( result[ 0 ] >= 2.0 && result[ 0 ] <= 2.1 )
+			|| result[ 1 ] >= 2.0 && result[ 1 ] <= 2.1 )
+		{
+			List.push( result );
+		}
+	}
+	
+	return List;
+}
